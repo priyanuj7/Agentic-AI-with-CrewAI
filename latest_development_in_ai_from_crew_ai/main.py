6b44a1283626899c84ff00bc0ca2911140491965
Priyanuj Misra <147880991+priyanuj7@users.noreply.github.com>
2025-02-02 20:25:48 +0400

    Add files via upload

---
+++ latest_development_in_ai_from_crew_ai/main.py
@@ -0,0 +1,22 @@
+import sys
+import warnings
+
+from crew import ai_latest_development
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs'
+    }
+    ai_latest_development().crew().kickoff(inputs=inputs)
+
+run()
